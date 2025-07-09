ee4cb722f4c8438043b2829cdfd9ec2237c474d6
lthsz <thomasglazarus.gamil.com>
2025-07-09 05:04:26 +0000

    Added the Numeric Data Type and Strings in the python file

---
+++ python-task/health_record_encoder.py
@@ -0,0 +1,11 @@
+                             # Health Record Encoder
+
+# 1. Numeric Data Types
+age = 29                        
+temperature = 36.5              
+unique_id = complex(3, 5)       
+
+# 2. Strings
+patientName = "Davelyn Gitayee"
+reportTitle = f"Health Report for {patientName}"
+
